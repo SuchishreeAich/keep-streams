367d4e62ec7956ebe7573647e0d3a42638d2146e
Ubuntu <ubuntu@ip-172-31-30-229.ec2.internal>
2019-03-05 14:46:13 +0530

    Node Streams

--- test/notesstream.spec.js
+++ test/notesstream.spec.js
@@ -4,10 +4,17 @@ const app = require('../app');
 const config = require('./test.config');
 
 
-describe('STreams test', () =>{
+describe('Streams test', () =>{
 
     it('Read notes as stream', (done)=> {
+        request(app)
+        .get(`/api/v1/notes`)
+        .send(config.USER_ID)
+        .expect(200)
+        .then((response)=>{
+        expect(response.body).to.have.property('notes');
         done();
+        });
     });
 
     it('upload notes as stream to db', (done)=> {
